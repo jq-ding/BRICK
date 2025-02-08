7b02621c8ff667cf4c9d63b17c7628534f30f657
Jiaqi Ding <93422935+jq-ding@users.noreply.github.com>
2025-02-08 15:07:16 -0500

    Update utils.py

--- source/utils.py
+++ source/utils.py
@@ -3,29 +3,20 @@ import os
 from torch.optim.lr_scheduler import _LRScheduler
 
 class LinearWarmupScheduler(_LRScheduler):
-    def __init__(self, optimizer, warmup_iters, last_iter=-1):
+    def __init__(self, optimizer, warmup_iters, last_epoch=-1):
         self.warmup_iters = warmup_iters
-        self.current_iter = 0 if last_iter == -1 else last_iter
         self.base_lrs = [group["lr"] for group in optimizer.param_groups]
-        super(LinearWarmupScheduler, self).__init__(optimizer, last_epoch=last_iter)
+        super().__init__(optimizer, last_epoch=last_epoch)
 
     def get_lr(self):
-        if self.current_iter < self.warmup_iters:
-            return [
-                base_lr * (self.current_iter + 1) / self.warmup_iters
-                for base_lr in self.base_lrs
-            ]
+        current_iter = max(self.last_epoch, 0)
+        if current_iter < self.warmup_iters:
+            return [base_lr * (current_iter + 1) / self.warmup_iters for base_lr in self.base_lrs]
         else:
-            return [base_lr for base_lr in self.base_lrs]
+            return self.base_lrs
 
-    def step(self, it=None):
-        if it is None:
-            it = self.current_iter + 1
-        self.current_iter = it
-        super(LinearWarmupScheduler, self).step(it)
         
 def compute_weighted_metrics(preds, gts):
-
     num_classes = len(torch.unique(gts))
     device = preds.device
 
